ebcac7eb8e9efb20f84e7e257411147bc2ea2bac
Darpan Chakrabarti <darpanchakrabarti@gmail.com>
2025-06-21 21:34:10 +0530

    Trajectory Comparison.py

--- Trajectory Comparison.py
+++ Trajectory Comparison.py
@@ -1,56 +1,67 @@
+
 import numpy as np
 import matplotlib.pyplot as plt
 
+# Gravitational constant and planetary parameters
+G = 6.674e-11           # Gravitational constant (m^3 kg^-1 s^-2)
+M = 5.972e24            # Mass of the Earth (kg)
+mu = G * M              # Standard gravitational parameter (m^3 s^-2)
+planet_radius = 6.371e6 # Radius of the Earth (m)
+# Time step and duration
+dt = 1                  # Time step (s)
+n_steps = 10000         # Number of steps
+x0 = -5e7               # Initial x-position (m), far left from the planet
+vy0 = 0                 # Initial y-velocity (m/s), no vertical velocity
 
-G = 6.674e-11          
-M = 5.972e24            
-mu = G * M             
-planet_radius = 6.371e6
-dt = 1               
-n_steps = 10000         
-b = 8.973e6         
-x0 = -5e7
-vy0 = 0
-planet_radius = 6.371e6
+def algo(b, vinf):
 
-def algo(b, vinf):    #Defining the function algo
+    # Initial conditions for the spacecraft
+    y0 = b                  # Initial y-position (m), equal to impact parameter
 
-    y0 = b                             
+    # Compute radial distance and initial x-velocity using energy conservation
     ro = (x0**2 + y0**2)**0.5
-    
     vx0 = (vinf**2 + (2*mu)/ro)**0.5
-               
+
+    # Initialize position and velocity lists
     x = [x0]
     y = [y0]
     vx = [vx0]
     vy = [vy0]
-    
+
+    # Numerical integration using Euler's method
     for i in range(n_steps):
-        r = np.sqrt(x[-1]**2 + y[-1]**2)
-        ax = -mu * x[-1] / r**3
-        ay = -mu * y[-1] / r**3
-    
-        vx_new = vx[-1] + ax * dt
-        vy_new = vy[-1] + ay * dt
-    
-        x_new = x[-1] + vx_new * dt
-        y_new = y[-1] + vy_new * dt
-    
+        r = np.sqrt(x[-1]**2 + y[-1]**2)              # Distance from the planet
+        ax = -mu * x[-1] / r**3                       # Acceleration in x due to gravity
+        ay = -mu * y[-1] / r**3                       # Acceleration in y due to gravity
+
+        vx_new = vx[-1] + ax * dt                     # Update velocity in x
+        vy_new = vy[-1] + ay * dt                     # Update velocity in y
+
+        x_new = x[-1] + vx_new * dt                   # Update position in x
+        y_new = y[-1] + vy_new * dt                   # Update position in y
+
+        # Append updated values to the lists
         x.append(x_new)
         y.append(y_new)
         vx.append(vx_new)
         vy.append(vy_new)
     
-    return x, y #Returning the computed lists of x and y values
+    return x, y # Returning the list of x and y values
 
+# Cassini
+vinf = 16.01e3          # Asymptotic incoming speed (m/s)
+b = 8.973e6             # Impact parameter (m)
+x1 , y1 = algo(b, vinf)
 
-x1, y1 = algo(8.973e6, 16.01e3) #Cassini
-x2, y2 = algo(11.261e6, 8.949e3) #Galileo I
+# Galileo I
+vinf = 8.949e3          # Asymptotic incoming speed (m/s)
+b = 11.261e6             # Impact parameter (m)
+x2 , y2 = algo(b, vinf)
 
 # Plot the trajectory
 plt.figure(figsize=(8, 8))
-plt.plot(np.array(x1)/1e3, np.array(y1)/1e3, label='Trajectory for Cassini')  
-plt.plot(np.array(x2)/1e3, np.array(y2)/1e3, label='Trajectory for Galileo I')  
+plt.plot(np.array(x1)/1e3, np.array(y1)/1e3, label='Trajectory')  # Position in km
+plt.plot(np.array(x2)/1e3, np.array(y2)/1e3, label='Trajectory') 
 
 # Add the planet as a filled circle
 circle = plt.Circle((0,0),planet_radius/1e3,color='r',alpha=0.2,label='Planet Surface')
