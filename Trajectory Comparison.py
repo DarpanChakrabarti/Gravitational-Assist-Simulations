e1205a5f45aa657d90da7206df964f62907be3b3
Darpan Chakrabarti <darpanchakrabarti@gmail.com>
2025-06-22 18:18:23 +0530

    Trajectory Comparison.py

--- Trajectory Comparison.py
+++ Trajectory Comparison.py
@@ -1,4 +1,3 @@
-
 import numpy as np
 import matplotlib.pyplot as plt
 
@@ -8,9 +7,9 @@ M = 5.972e24            # Mass of the Earth (kg)
 mu = G * M              # Standard gravitational parameter (m^3 s^-2)
 planet_radius = 6.371e6 # Radius of the Earth (m)
 # Time step and duration
-dt = 1                  # Time step (s)
-n_steps = 10000         # Number of steps
-x0 = -5e7               # Initial x-position (m), far left from the planet
+dt = 0.01                  # Time step (s)
+n_steps = int(7e6)         # Number of steps
+x0 = -5e8               # Initial x-position (m), far left from the planet
 vy0 = 0                 # Initial y-velocity (m/s), no vertical velocity
 
 def algo(b, vinf):
@@ -30,15 +29,15 @@ def algo(b, vinf):
 
     # Numerical integration using Euler's method
     for i in range(n_steps):
-        r = np.sqrt(x[-1]**2 + y[-1]**2)              # Distance from the planet
-        ax = -mu * x[-1] / r**3                       # Acceleration in x due to gravity
-        ay = -mu * y[-1] / r**3                       # Acceleration in y due to gravity
+        r = np.sqrt(x[-1]**2 + y[-1]**2)            # Distance from the planet
+        ax = -mu * x[-1] / r**3                     # Acceleration in x due to gravity
+        ay = -mu * y[-1] / r**3                     # Acceleration in y due to gravity
 
-        vx_new = vx[-1] + ax * dt                     # Update velocity in x
-        vy_new = vy[-1] + ay * dt                     # Update velocity in y
+        vx_new = vx[-1] + ax * dt                   # Update velocity in x
+        vy_new = vy[-1] + ay * dt                   # Update velocity in y
 
-        x_new = x[-1] + vx_new * dt                   # Update position in x
-        y_new = y[-1] + vy_new * dt                   # Update position in y
+        x_new = x[-1] + vx_new * dt                 # Update position in x
+        y_new = y[-1] + vy_new * dt                 # Update position in y
 
         # Append updated values to the lists
         x.append(x_new)
@@ -50,18 +49,18 @@ def algo(b, vinf):
 
 # Cassini
 vinf = 16.01e3          # Asymptotic incoming speed (m/s)
-b = 8.973e6             # Impact parameter (m)
+b = 8.973e6             # Impact parameter (m) 
 x1 , y1 = algo(b, vinf)
 
 # Galileo I
 vinf = 8.949e3          # Asymptotic incoming speed (m/s)
-b = 11.261e6             # Impact parameter (m)
+b = 11.261e6             # Impact parameter (m) 
 x2 , y2 = algo(b, vinf)
 
 # Plot the trajectory
 plt.figure(figsize=(8, 8))
-plt.plot(np.array(x1)/1e3, np.array(y1)/1e3, label='Trajectory')  # Position in km
-plt.plot(np.array(x2)/1e3, np.array(y2)/1e3, label='Trajectory') 
+plt.plot(np.array(x1)/1e3, np.array(y1)/1e3, label='Trajectory of Cassini')  # Position in km
+plt.plot(np.array(x2)/1e3, np.array(y2)/1e3, label='Trajectory of Galileo I')  # Position in km
 
 # Add the planet as a filled circle
 circle = plt.Circle((0,0),planet_radius/1e3,color='r',alpha=0.2,label='Planet Surface')
